fcabce7222b22c9ab35ba1ab7386b6cc25f127e7
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-07 12:47:32 -0500

    Handling errors in get all artist use case

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -3,12 +3,17 @@ const pagination = require("../helpers/pagination");
 const ArtistLister = require("../core/artist/aplication/artist_lister");
 const InMemoryArtistRepository = require("../core/artist/infrastructure/in_memory_artist_repository");
 exports.list = async function (req, res) {
-  const artistLister = new ArtistLister(new InMemoryArtistRepository());
-  const options = pagination.getPaginationOptions(req);
-  const artist = await artistLister.call(options.page, options.limit);
-  pagination.setPaginationHeaders(res, {
-    page: options.page,
-    limit: options.limit,
-  });
-  res.json(artist);
+  try {
+    const artistLister = new ArtistLister({});
+    const options = pagination.getPaginationOptions(req);
+    const artist = await artistLister.call(options.page, options.limit);
+    pagination.setPaginationHeaders(res, {
+      page: options.page,
+      limit: options.limit,
+    });
+    res.json(artist);
+  } catch (error) {
+    console.log(error);
+    res.status(error.responseCode).json({ message: error.message });
+  }
 };
