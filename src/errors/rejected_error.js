73d89b401a06a3504444c9244efdb7f6481a07af
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-07 16:37:58 -0500

    Removing unnecesary error message

--- src/errors/rejected_error.js
+++ src/errors/rejected_error.js
@@ -2,7 +2,7 @@ const ResponseError = require("./response_error");
 
 class RejectedError extends ResponseError {
   constructor(error) {
-    super(420, error? error :"Your request is already rejected");
+    super(420, error);
   }
 }
 
