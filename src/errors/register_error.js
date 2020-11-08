08af84df74807b1407e611c19f267747c8fc8731
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-07 22:42:25 -0500

    Removing unused error

--- src/errors/register_error.js
+++
@@ -1,9 +0,0 @@
-const ResponseError = require("./response_error");
-
-class RegisterError extends ResponseError {
-    constructor() {
-        super(400, "Could not register what was requested");
-    }
-}
-
-module.exports = RegisterError;
