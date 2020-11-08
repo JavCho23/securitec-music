07ec6d9f6524223ea57b7feb4585321d6055e78a
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-08 11:16:39 -0500

    Adding sum of all songs duration for a album

--- src/core/album/domain/album.js
+++ src/core/album/domain/album.js
@@ -26,8 +26,42 @@ class Album {
 
   calculateDetails(songs) {
     // TODO add sum of total duration
-    this.totalDuration = "oe";
+    this.totalDuration = songs.reduce((acumulator, current) => {
+      return this.addDurations(acumulator.duration, current.duration);
+    });
     this.totalSongs = songs.length;
+  }
+  addDurations(duration, anotherDuration) {
+    if (!duration) return anotherDuration;
+    let resultado = [];
+    duration = duration.split(":").reverse();
+    anotherDuration = anotherDuration.split(":").reverse();
+    let acumulado = 0;
+    let sumaAcumulado;
+    let suma = parseInt(duration[0]) + parseInt(anotherDuration[0]);
+    sumaAcumulado = this.acumulate(suma, acumulado);
+    resultado.push(sumaAcumulado[0]);
+    suma =
+      parseInt(duration[1]) + parseInt(anotherDuration[1]) + sumaAcumulado[1];
+    acumulado = 0;
+    sumaAcumulado = this.acumulate(suma, acumulado);
+    resultado.push(sumaAcumulado[0]);
+    resultado.reverse();
+    return resultado.join(":");
+  }
+  acumulate(suma, acumulado) {
+    if (suma > 60) {
+      suma = suma - 60;
+      acumulado += 1;
+      this.acumulate(suma, acumulado);
+    }
+
+    if (suma == 60) {
+      suma = 0;
+      acumulado += 1;
+      this.acumulate(suma, acumulado);
+    }
+    return [suma, acumulado];
   }
 
   toJson() {
