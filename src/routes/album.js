ab26071ba78e0028b8c5d052b052b72c0b48a7c1
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-07 22:04:48 -0500

    Adding use update song use case to router

--- src/routes/album.js
+++ src/routes/album.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.route("/").get(album.list);
 routes.route("/:id").get(album.read).delete(album.delete);
 routes.route("/:id/songs").get(song.listByAlbum).post(song.create);
+routes.route("/:id/songs/:idSong").put(song.update);
 
 exports.routes = routes;
