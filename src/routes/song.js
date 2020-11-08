cd70cabcb80d549f7b15011be17b7b8e847ee49b
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-07 22:41:45 -0500

    Adding delete song use case to router

--- src/routes/song.js
+++ src/routes/song.js
@@ -3,6 +3,6 @@ const song = require("../controllers/song");
 const routes = express.Router();
 
 routes.route("/").get(song.list);
-routes.route("/:id").get(song.read);
+routes.route("/:id").get(song.read).delete(song.delete);
 
 exports.routes = routes;
