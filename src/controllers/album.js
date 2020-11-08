5654149a896a80298473305baba4a4839ec2c641
Javier Arturo Chávez Sialer <javier120699lili@gmail.com>
2020-11-07 22:10:57 -0500

    Rename album deleter file

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -7,7 +7,7 @@ const AlbumFinder = require("../core/album/aplication/album_finder");
 const AlbumCreator = require("../core/album/aplication/album_creator");
 const AlbumUpdater = require("../core/album/aplication/album_updater");
 const Album = require("../core/album/domain/album");
-const AlbumDeleter = require("../core/album/aplication/artist_deleter");
+const AlbumDeleter = require("../core/album/aplication/album_deleter");
 exports.list = async function (req, res) {
   try {
     const albumLister = new AlbumLister(new AlbumRepository());
